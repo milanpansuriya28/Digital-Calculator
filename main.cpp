d88c1770012c9787e6150e3f06a9c3ef819f3ec3
milanpansuriya28 <milanpansuriya2828@gmail.com>
2024-07-30 19:50:58 -0400

    add Trigonometry Calculator

--- main.cpp
+++ main.cpp
@@ -3,18 +3,19 @@
 
 using namespace std;
 
-class BasicCalculator{
-public:
-    //Function : Start Message
+//Function : Start Message
     void startMessage(){
         cout << "------------------------------------------" << endl;
-        cout << "\t Digital Calculatot" << endl;
+        cout << "\t Digital Calculator" << endl;
         cout << "------------------------------------------" << endl;
     }
 
-    //basic loop operations loop
+//----------------------------------------------------------------------Basic Calculator---------------------------------------------------------------------
+class BasicCalculator{
+public:
+    //basic operations loop 
     void basicOperationloop(){
-        bool code = true;
+        bool code = true; // condtion for loop
         double num1;
         cout << "Enter First number : ";
         cin >> num1;
@@ -79,7 +80,7 @@ private:
         }
     } 
 
-    //stop loop
+    //stop basic operations loop
     bool basicloopExit(bool& code, string choise){
         code = (choise != "1" );
         return code;
@@ -87,8 +88,11 @@ private:
 
 };
 
+//---------------------------------------------------------------Scientific Calculator---------------------------------------------------------------------
+
 class ScientificCalculator {
 public:
+    //Scientific operations loop 
      void ScientificOperationloop(){
          bool code = true;
         while (code) {
@@ -107,6 +111,7 @@ public:
         }
     }
 private:
+    //get user's choise which Scientific operations they want to do
     int sciUserChoice(){
         int sciChoise;
         cout << "X^2 - 1 \nx^3 - 2 \nx^n - 3 \n2^n - 4 \nExit = 0 " << endl;
@@ -116,6 +121,7 @@ private:
         return sciChoise;
     }
 
+    //Scientific operations
     double sciOperation(int sciChoise){
         if(sciChoise == 1){
             cout << "Enter \"x\" value :";
@@ -149,25 +155,130 @@ private:
             return NAN;
         }
     }
+
+    // stop Scientific operations loop
     bool sciLoopExit(bool& code, int choise){
         code = (choise != 0);
         return code;
     }
 };
 
+//------------------------------------------------------------------Trigonometry Calculator---------------------------------------------------------------------
+class TrigonometryCalculator{
+public:
+    //Trigonometry Operation loop
+    void TrigonometryOperationloop(){
+         bool code = true;
+        while (code) {
+            int func = getTrigonometryFunc();
+
+            trigonometryLoopExit(code, func);
+
+            if (!code) break;
+
+            double angle = getAngle();
+
+            double answer = trigonometryValues(angle, func);
+            if (!isnan(answer)) {
+                cout << "Result: " << answer << endl;
+            } else {
+                cout << "Operation failed. Retrying." << endl;
+            }
+        }
+    }
+private:
+    // Get Trigonometry Functions like sin, cos from user
+    int getTrigonometryFunc(){
+        int func;
+        cout << "Select Trigonometry functiom : \nSin   - 1 \nCos   - 2 \nTan   - 3 \nCosec - 4 \nSec   - 5 \nCot   - 6 \nExit  - 0" << endl;
+        cin >> func;
+        if(func<7){
+            return func;
+        }
+        else{
+            cout << "Invalid";
+            return NAN;
+        }
+    }
+
+    //Get angle from user in radians or degrees
+    double getAngle(){
+        int unitChoise;
+        double angle;
+        double radians;
+        double degrees;
+
+        cout << "Which units for measuring angles you want to input?" << endl;
+        cout << "Radians - 1 \nDegrees - 2" << endl;
+        cin >> unitChoise;
+        
+        if(unitChoise == 1){
+            cout << "Enter angle in Radians : ";
+            cin >> angle;
+        }
+        else{
+            cout << "Enter angle in Degrees : ";
+            cin >> degrees;
+            angle = degrees * (M_PI / 180.0);
+        }
+        return angle;
+    }
+
+    //Trigonometry Operation
+    double trigonometryValues(double& angle, int& func){
+        if(func == 1){
+            return sin(angle);
+        }
+        else if(func == 2){
+            return cos(angle);
+        }
+        else if(func == 3){
+           return tan(angle);
+        }
+        else if(func == 4){
+            return (1 / sin(angle));
+        }
+         else if(func == 5){
+            return (1 / cos(angle));
+        }
+         else{
+            return (1 / tan(angle));
+        }
+    }
+
+    //Stop Trigonometry Operation loop
+    bool trigonometryLoopExit(bool& code, int func){
+        code = (func != 0);
+        return code;
+    }
+};
+
 int main(){
-    BasicCalculator basicClac;
+    //make  Basic Calculator object
+    BasicCalculator basicClac; 
+
+    //make Scientific Calculator object
     ScientificCalculator scientificClac;
-    basicClac.startMessage();
+
+    //make Trigonometry Calculator object
+    TrigonometryCalculator trigonometryClac;
+
+    //Start message
+    startMessage();
+    
     char calcType;
-    cout << "Choose calculator type: Basic (b) or Scientific (s): ";
+    cout << "Choose calculator type: \nBasic (b)  \nScientific (s) \nTrigonometry (t) \n: ";
     cin >> calcType;
     
     if(calcType == 'b' || calcType == 'B'){
-    basicClac.basicOperationloop();
+        basicClac.basicOperationloop();
     }
-    else if(calcType == 's' || calcType == 'S')
-    scientificClac.ScientificOperationloop();
+    else if(calcType == 's' || calcType == 'S'){
+        scientificClac.ScientificOperationloop();
+    }
+    else if(calcType == 't' || calcType == 'T'){
+        trigonometryClac.TrigonometryOperationloop();
+    }
     else{
         cout << "Invalid Choise!";
     }
